c16b142a0878341ec6da49a9d9970f06cbf280b5
xuheng <zidiaolin@163.com>
2018-12-19 20:52:53 +0800

    创建

---
+++ test1-2018.py
@@ -0,0 +1,10 @@
+class Calc:
+    name="计算器"
+    def add(self,x,y):
+        print(x+y)
+    def minus(self,x,y):
+        print(x-y)
+    def times(self,x,y):
+        print(x*y)
+    def divide(self,x,y):
+        print(x/y)
