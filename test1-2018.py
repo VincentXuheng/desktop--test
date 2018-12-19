15ea1a063337194735eaae3a0117eee84e59b683
xuheng <zidiaolin@163.com>
2018-12-19 21:25:38 +0800

    字典用法

--- test1-2018.py
+++ test1-2018.py
@@ -1,10 +1,2 @@
-class Calc:
-    name="计算器"
-    def add(self,x,y):
-        print(x+y)
-    def minus(self,x,y):
-        print(x-y)
-    def times(self,x,y):
-        print(x*y)
-    def divide(self,x,y):
-        print(x/y)
+d={'1':[1,2,3],'2':{1:11,2:22},'3':88}
+print(d['2'][1])
